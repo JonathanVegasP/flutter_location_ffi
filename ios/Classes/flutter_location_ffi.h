6712c1aac876fc47aa70ef09ac77a8687546f763
Jonathan Vegas Peixoto <jonathan.v.peixoto@gmail.com>
2025-07-18 04:29:25 -0300

    feat: added full support for ios

---
+++ ios/Classes/flutter_location_ffi.h
@@ -0,0 +1,12 @@
+//
+//  flutter_location_ffi.h
+//  Pods
+//
+//  Created by Leticia Rodrigues Vegas on 17/07/25.
+//
+
+#ifndef flutter_location_ffi_h
+#define flutter_location_ffi_h
+
+
+#endif /* flutter_location_ffi_h */
